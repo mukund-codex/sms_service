978669154721722098da4d3b3dc354d5b07082ae
Mukunda Vishwakarma <mukunda@techizer.in>
2019-11-08 15:28:05 +0530

    FIX :: Added migrations

---
+++ database/migrations/2019_11_08_094811_sms_request_log.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class SmsRequestLog extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema:;create('sms_request_log', function(Blueprint $table){
+            $table->integer('id')->autoIncrement();
+            $table->string('uid');
+            $table->string('request_id');
+            $table->string('to');
+            $table->string('message');
+            $table->string('sender_id');
+            $table->string('provider');
+            $table->string('callback');
+            $table->string('status');
+            $table->string('error');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::dropIfExists('sms_request_log');
+    }
+}
---
+++ database/migrations/2019_11_08_094837_sms_log.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class SmsLog extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('sms_log', function(Blueprint $table){
+            $table->integer('id')->autoIncrement();
+            $table->string('uid');
+            $table->string('request_id');
+            $table->string('to');
+            $table->string('message');
+            $table->string('sender_id');
+            $table->string('provider');
+            $table->string('callback');
+            $table->string('is_success');
+            $table->string('output');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::dropIfExists('sms_log');
+    }
+}
